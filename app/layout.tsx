a28426186dde3a3504fd94c10e1ad6e0aafc27ce
APPAH OYINMIEBI JOSEPH <109265761+miebi1@users.noreply.github.com>
2026-02-12 22:39:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,10 @@ export default async function RootLayout({
   const session = await auth()
 
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <SessionProvider session={session}>
           <CartProvider>
